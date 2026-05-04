27322b9b32d44597bc487895d51c3eb9e736a93b
Mahfooz Alam <ma.mahfoozalam8789@gmail.com>
2026-05-05 00:27:38 +0530

    Fix comments in bitwise operations output

--- DayOne/bitwiseOp.java
+++ DayOne/bitwiseOp.java
@@ -4,8 +4,8 @@ public class bitwiseOp {
         int a = 5;
         int b = 3;
         
-        System.out.println(a | b); // 1 1 -> 1, 0 0 -> 0, 1 0 -> 0
-        System.out.println(a & b); // 1 1 -> 1, 1 0 -> 1, 0 0 -> 0
+        System.out.println(a | b); // 1 1 -> 1, 1 0 -> 1, 0 0 -> 0 
+        System.out.println(a & b); // 1 1 -> 1, 0 0 -> 0, 1 0 -> 0
         System.out.println(a ^ b); // 1 0 -> 1 , 1 1 (or) 0 0 -> 0
         System.out.println(~a);
      }
